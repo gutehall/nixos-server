f07b60ac0b6a2086bd25462d592e42c535ec0f87
gutehall <mathias@dm3.io>
2024-05-01 11:14:23 +0200

    Added support for storing config outside of /etc/nixos

--- setup.sh
+++ setup.sh
@@ -7,12 +7,22 @@ export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 nix-shell '<home-manager>' -A install
 
 # Replace the configuration files with those from the repository
-sudo rm -rf /etc/nixos/configuration.nix
-sudo cp *.nix /etc/nixos/
+# sudo rm -rf /etc/nixos/configuration.nix
+# sudo cp *.nix /etc/nixos/
+
+# Insert support for the configuration
+config_to_insert='
+nix.nixPath = [
+  "nixos-config=/home/mathias/nixos"
+];
+'
+
+# Insert the configuration into the file
+sudo sed -i "/^}/i $config_to_insert" "/etc/nixos/configuration.nix"
 
 # Optionally, you can customize the configuration files here
 # For example:
 # sed -i 's/original_text/replacement_text/g' /etc/nixos/configuration.nix
 
 # Activate the new configuration
-sudo nixos-rebuild switch
+sudo nixos-rebuild switch --use-remote-sudo -I nixos-config=/home/mathias/nixos
